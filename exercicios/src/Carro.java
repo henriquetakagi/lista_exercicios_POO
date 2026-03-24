8d5d51fb73d5678bdaeb9dd87f54fd37f76cd667
henriquetakagi <henrique.takagi@aluno.senai.br>
2026-03-23 20:59:10 -0400

    Exercicio 2 - Método void vs método com retorno

--- exercicios/src/Carro.java
+++ exercicios/src/Carro.java
@@ -8,12 +8,17 @@ public class Carro {
         this.modelo = modelo;
         this.ano = ano;
     }
-    public void ImprimirDados(){
+    public void ImprimirDados(){//ou exibir() faz a mesma coisa
         System.out.println("- Dados do Carro -");
         System.out.println("Marca :" + marca);
         System.out.println("Modelo :" + modelo);
         System.out.println("Ano :" + ano);
+        System.out.println("Idade :" + idadeDoCarro());
     }
+
+    public int idadeDoCarro(){
+        return 2026 - ano;
+    }// Essa funcao retorna um valor int e executa apenas uma açao
 
 
 }
