f9c78c5f6df71e620b377948e2caadc9fd30820e
henriquetakagi <henrique.takagi@aluno.senai.br>
2026-03-23 21:04:24 -0400

    Exercício 3 – ArrayList e for-each

--- exercicios/src/Main.java
+++ exercicios/src/Main.java
@@ -1,9 +1,17 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
+        ArrayList<Carro> lista_carro = new ArrayList<Carro>();
+
         Carro carro1 = new Carro("Honda","HR-V", 2025);
         Carro carro2 = new Carro("Nissan","GT-R", 2011);
-        carro1.ImprimirDados();
-        carro2.ImprimirDados();
+        lista_carro.add(carro1);
+        lista_carro.add(carro2);
+        for(int i = 0; i < lista_carro.size(); i++){
+            lista_carro.get(i).ImprimirDados();
+        }
     }
 }
 
